44c52d71ed7dacc80e840123b9272b59f542940d
Hugo van Rijswijk <git@hugovr.nl>
2023-05-10 12:11:54 +0200

    Remove node-fetch and use pnpm

--- HttpJS.scala
+++ HttpJS.scala
@@ -1,40 +1,12 @@
 import cats.effect.{Async, Resource, Sync}
 import cats.syntax.apply.*
-import org.legogroup.woof.{Logger, given}
 import sttp.client4.impl.cats.FetchCatsBackend
 
 import scala.scalajs.js
 import scala.scalajs.js.annotation.*
+import sttp.client4.WebSocketBackend
 
-def httpBackend[F[_]](using F: Async[F], log: Logger[F]) = Resource
-  .pure(FetchCatsBackend[F]())
-  .evalTap(_ =>
-    F.defer {
-      val g = js.Dynamic.global.globalThis
-
-      if js.isUndefined(g.fetch) then
-        F.fromPromise(F.delay(js.dynamicImport {
-          g.fetch = NodeFetch.default
-          g.Headers = NodeFetch.Headers
-          g.Request = NodeFetch.Request
-          g.Response = NodeFetch.Response
-        })) *> log.debug("polyfilled fetch with node-fetch")
-      else log.debug("fetch already defined, not overwriting with node-fetch")
-      end if
-    }
-  )
-
-@js.native
-trait NodeFetch extends js.Object:
-  val default: js.Any  = js.native
-  val Headers: js.Any  = js.native
-  val Request: js.Any  = js.native
-  val Response: js.Any = js.native
-end NodeFetch
-
-@js.native
-@JSImport("node-fetch", JSImport.Namespace)
-object NodeFetch extends NodeFetch
+def httpBackend[F[_]](using F: Async[F]): Resource[F, WebSocketBackend[F]] = Resource.pure(FetchCatsBackend[F]())
 
 def loadConfig[F[_]](using F: Sync[F]): F[Unit] = F.delay {
   Dotenv.config()
