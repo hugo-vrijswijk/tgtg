8aad636687574a7d24a37aa6c37853927333e4be
Hugo van Rijswijk <git@hugovr.nl>
2023-09-19 15:23:47 +0200

    chore(deps): dependency updates

--- project.scala
+++ project.scala
@@ -1,13 +1,13 @@
 //> using scala "3"
 //> using lib "org.typelevel::cats-effect::3.5.1"
-//> using lib co.fs2::fs2-core::3.9.1
-//> using lib co.fs2::fs2-io::3.9.1
+//> using lib co.fs2::fs2-core::3.9.2
+//> using lib co.fs2::fs2-io::3.9.2
 //> using lib "com.softwaremill.sttp.client4::circe::4.0.0-M4"
 //> using lib "com.softwaremill.sttp.client4::cats::4.0.0-M4"
 //> using lib "io.chrisdavenport::rediculous::0.5.0"
 //> using lib "io.circe::circe-core::0.14.6"
 //> using lib "io.circe::circe-parser::0.14.6"
-//> using lib "org.legogroup::woof-core::0.6.1"
+//> using lib "org.legogroup::woof-core::0.7.0"
 //> using lib com.monovore::decline-effect::2.4.1
 
 //> using jsModuleKind "es"
