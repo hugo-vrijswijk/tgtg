1448ebbb85d3eb3b8c6882827e3255f6e9c39264
Hugo van Rijswijk <git@hugovr.nl>
2024-04-30 13:11:32 +0200

    Update deps

--- Model.scala
+++ Model.scala
@@ -4,7 +4,7 @@ import cats.Show
 import cats.effect.IO
 import cats.syntax.all.*
 import fs2.io.file.{Flags, Path}
-import io.circe.{Codec, Decoder, Encoder, JsonObject}
+import io.circe.{Codec, Decoder, Encoder}
 
 import java.util.concurrent.TimeUnit
 import scala.concurrent.duration.FiniteDuration
@@ -26,7 +26,7 @@ case class GetItemsRequest(
     // we_care_only: Boolean = false
 ) derives Encoder.AsObject
 
-case class Origin(latitude: Long = 0L, longitude: Long = 0L)
+case class Origin(latitude: Long = 0L, longitude: Long = 0L) derives Encoder.AsObject
 
 case class RefreshRequest(refresh_token: ApiToken) derives Encoder.AsObject
 case class RefreshResponse(
@@ -40,7 +40,7 @@ case class AccessToken(cookies: Cookies, access_token: ApiToken, ttl: FiniteDura
 
 case class GetItemsResponse(items: Seq[ItemWrapper]) derives Decoder
 
-case class Price(code: String, minor_units: Int, decimals: Int)
+case class Price(code: String, minor_units: Int, decimals: Int) derives Decoder
 
 given Show[Price] = Show.show: price =>
   val currencySymbol = price.code match
@@ -55,9 +55,9 @@ case class ItemWrapper(item: Item, store: Store, items_available: Int, display_n
 given Show[ItemWrapper] = i =>
   show"${i.store.store_name} has ${if i.item.name.isBlank() then "box" else i.item.name} available for ${i.item.item_price}"
 
-case class Item(name: String, item_price: Price)
+case class Item(name: String, item_price: Price) derives Decoder
 
-case class Store(store_name: String)
+case class Store(store_name: String) derives Decoder
 
 case class LoginRequest(email: Email, device_type: String = "ANDROID") derives Encoder.AsObject
 case class LoginResponse(state: String, polling_id: Option[String]) derives Decoder
--- project.scala
+++ project.scala
@@ -2,11 +2,11 @@
 //> using dep "org.typelevel::cats-effect::3.5.4"
 //> using dep co.fs2::fs2-core::3.10.2
 //> using dep co.fs2::fs2-io::3.10.2
-//> using dep "com.softwaremill.sttp.client4::circe::4.0.0-M11"
-//> using dep "com.softwaremill.sttp.client4::cats::4.0.0-M11"
+//> using dep "com.softwaremill.sttp.client4::circe::4.0.0-M13"
+//> using dep "com.softwaremill.sttp.client4::cats::4.0.0-M13"
 //> using dep "io.chrisdavenport::rediculous::0.5.1"
-//> using dep "io.circe::circe-core::0.14.6"
-//> using dep "io.circe::circe-parser::0.14.6"
+//> using dep "io.circe::circe-core::0.14.7"
+//> using dep "io.circe::circe-parser::0.14.7"
 //> using dep "org.legogroup::woof-core::0.7.0"
 //> using dep com.monovore::decline-effect::2.4.1
 
