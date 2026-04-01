ab60403a732c43800d855693739ae77dd250efd5
Scala Steward <scala_steward@virtuslab.com>
2026-04-01 15:45:00 +0000

    Update scala3-library to 3.8.3

--- project.scala
+++ project.scala
@@ -1,4 +1,4 @@
-//> using scala "3.8.1"
+//> using scala "3.8.3"
 //> using dep "co.fs2::fs2-core::3.13.0"
 //> using dep "co.fs2::fs2-io::3.13.0"
 //> using dep "com.monovore::decline-effect::2.6.1"
